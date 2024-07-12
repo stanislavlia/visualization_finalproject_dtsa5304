7ce30765a5929e42495b9f0b1d01e1241ed66877
stanislavlia <stanislavliashkov@gmail.com>
2024-07-12 14:52:57 +0700

    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def build_price_histogram(filtered_df):
               axis=alt.Axis(values=list(range(0, int(max_price) + 1, 5)))),
         alt.Y('fraction:Q', title='Percentage', axis=alt.Axis(format='.2%'),
               scale=alt.Scale(domain=[0, 0.60])),
-        tooltip=[alt.Tooltip('binned_price:Q', title='Price(pounds)'), alt.Tooltip('fraction:Q', format='.3f', title='fration')]
+        tooltip=[alt.Tooltip('binned_price:Q', title='Price(pounds)'), alt.Tooltip('fraction:Q', format='.3f', title='fraction')]
     ).properties(
         width=600,  
         height=400,  
